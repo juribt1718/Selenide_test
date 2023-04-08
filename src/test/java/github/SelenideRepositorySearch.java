08fe82227d25f80192c2b39683b1b5de6346e009
Bilov Yuriy <jurib@rambler.ru>
2023-04-08 23:22:15 +0300

    first worked test

--- src/test/java/github/SelenideRepositorySearch.java
+++ src/test/java/github/SelenideRepositorySearch.java
@@ -1,22 +1,34 @@
 package github;
 
+import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import org.apache.commons.lang3.builder.ToStringExclude;
-import org.junit.Test;
+//import org.junit.Test;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selenide.*;
 
 public class SelenideRepositorySearch {
 
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        // Configuration.baseUrl = "https://demoqa.com";
+        System.setProperty("chromeoptions.args", "--remote-allow-origins=*");
+    }
 
 @Test
 void shouldFindSelenideRepositoryAtTheTop(){
+
 
     //Открыть главную страницу
     open("https://github.com/");
 
     //Ввести в поиск selenide и нажать Enter
 $("[placeholder ='Search GitHub']").setValue("selenide").pressEnter();
+
+sleep(5000);
 
     }
 }
